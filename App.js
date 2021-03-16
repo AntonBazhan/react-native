c2d1352765e5a4cd72e9f0ffbb3ff59df569809e
Anton <bazhan.anton@ukr.net>
2021-03-16 10:20:22 +0200

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {} from "react-native";
+
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { useRoute } from "./router";
+
+const loadApplication = async () => {
+  await Font.loadAsync({
+    "DMMono-Regular": require("./assets/fonts/DMMono-Regular.ttf"),
+  });
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [iasReady, setIasReady] = useState(false);
+  const routing = useRoute(true);
+  if (!iasReady) {
+    return (
+      <AppLoading
+        startAsync={loadApplication}
+        onFinish={() => setIasReady(true)}
+        onError={console.warn}
+      />
+    );
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <NavigationContainer>{routing}</NavigationContainer>;
+}
